75c30c1c470811ac9572d7d04a31369334e45884
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-11-19 19:34:33 +0100

    Try to use emotion to style button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { css } from '@emotion/react';
 
 export const Button = ({ text, ...props }) => (
-  <button {...props}>{text}</button>
+  <button
+    {...props}
+    css={css`
+      padding: 32px;
+      background-color: hotpink;
+      font-size: 24px;
+      border-radius: 4px;
+    `}>{text}</button>
 )
